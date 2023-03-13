c6273b86616fce6df2db3254bf41984340ef8560
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-03-14 00:00:05 +0100

    Style card finished + framer motion anims

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,65 @@
-import React from "react";
+import countries from "i18n-iso-countries";
+import React, { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
-const Movie = ({ movie }) => {
+const Movie = ({ movie, moviesGenres, setSelectedId, index }) => {
+  const dateFormat = (date) => {
+    const dateObj = new Date(date);
+    const dateFormat = dateObj.toLocaleString("fr-FR", {
+      year: "numeric",
+      month: "short",
+    });
+    const output = dateFormat;
+    return output;
+  };
+
   return (
-    <div id={movie.id} className="home-card">
-      <img
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-        alt=""
-      />
-      <h3>{movie.title}</h3>
-    </div>
+    <>
+      <motion.div
+        id={movie.id}
+        className="movie-card"
+        onClick={() => setSelectedId(movie.id)}
+        initial={{ opacity: 0, y: 50 }}
+        animate={{
+          transition: { delay: index * 0.06 },
+          opacity: 1,
+          y: 0,
+        }}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt={`Poster du film : ${movie.title}`}
+        />
+        <h3>{movie.title}</h3>
+        <div className="movie-infos">
+          <div className="origin">
+            <p>
+              Sortie :<bold> {dateFormat(movie.release_date)}</bold>
+            </p>
+            <p>
+              {movie.original_language === "en"
+                ? "Etats Unis"
+                : countries.getName(movie.original_language, "fr")}
+            </p>
+          </div>
+          <div className="genre">
+            <p>Catégories :</p>
+            <ul>
+              {movie.genre_ids.map((genre, index) => {
+                return (
+                  <li key={genre}>
+                    {moviesGenres.map((genreName) => {
+                      if (genre === genreName.id) return genreName.name;
+                      else return null;
+                    })}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </motion.div>
+    </>
   );
 };
 
