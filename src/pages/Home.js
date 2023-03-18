cefe98608b0bbc03b6db4040134d63758a92c317
jonathanm98 <jonathanmenelet@hotmail.fr>
2023-03-18 05:06:45 +0100

    nothing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { AnimatePresence } from "framer-motion";
 import React, { useState, useEffect } from "react";
 import ActiveMovieCard from "../components/ActiveMovieCard";
 import Header from "../components/Header";
